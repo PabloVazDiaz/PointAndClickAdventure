cd50f025802477aa61161522feb8b3f2b5ce950b
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2018-08-02 13:22:36 +0200

    Inventory

--- Assets/Scripts/Editor/Inventory/InventoryEditor.cs
+++ Assets/Scripts/Editor/Inventory/InventoryEditor.cs
@@ -1,52 +1,49 @@
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(Inventory))]
+[CustomEditor (typeof(Inventory))]
 public class InventoryEditor : Editor
 {
-    private bool[] showItemSlots = new bool[Inventory.numItemSlots];
     private SerializedProperty itemImagesProperty;
     private SerializedProperty itemsProperty;
+    private bool[] showItemsSlots = new bool[Inventory.numItemSlots];
 
 
     private const string inventoryPropItemImagesName = "itemImages";
-    private const string inventoryPropItemsName = "items";
+    private const string inventoryPropItemName = "items";
 
-
-    private void OnEnable ()
+    private void OnEnable()
     {
-        itemImagesProperty = serializedObject.FindProperty (inventoryPropItemImagesName);
-        itemsProperty = serializedObject.FindProperty (inventoryPropItemsName);
+        itemImagesProperty = serializedObject.FindProperty(inventoryPropItemImagesName);
+        itemsProperty = serializedObject.FindProperty(inventoryPropItemName);
     }
 
-
-    public override void OnInspectorGUI ()
+    public override void OnInspectorGUI()
     {
-        serializedObject.Update ();
+        serializedObject.Update();
 
         for (int i = 0; i < Inventory.numItemSlots; i++)
         {
-            ItemSlotGUI (i);
+            ItemSlotGUI(i);
         }
 
-        serializedObject.ApplyModifiedProperties ();
+        serializedObject.ApplyModifiedProperties();
     }
 
-
-    private void ItemSlotGUI (int index)
+    private void ItemSlotGUI(int index)
     {
-        EditorGUILayout.BeginVertical (GUI.skin.box);
+        EditorGUILayout.BeginVertical(GUI.skin.box);
         EditorGUI.indentLevel++;
-        
-        showItemSlots[index] = EditorGUILayout.Foldout (showItemSlots[index], "Item slot " + index);
 
-        if (showItemSlots[index])
+        showItemsSlots[index]= EditorGUILayout.Foldout(showItemsSlots[index],"Item Slot "+ index);
+
+        if (showItemsSlots[index])
         {
-            EditorGUILayout.PropertyField (itemImagesProperty.GetArrayElementAtIndex (index));
-            EditorGUILayout.PropertyField (itemsProperty.GetArrayElementAtIndex (index));
+            EditorGUILayout.PropertyField(itemImagesProperty.GetArrayElementAtIndex(index));
+            EditorGUILayout.PropertyField(itemsProperty.GetArrayElementAtIndex(index));
         }
 
         EditorGUI.indentLevel--;
-        EditorGUILayout.EndVertical ();
+        EditorGUILayout.EndVertical();
     }
 }
--- Assets/Scripts/MonoBehaviours/Inventory/Inventory.cs
+++ Assets/Scripts/MonoBehaviours/Inventory/Inventory.cs
@@ -3,18 +3,17 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    public Image[] itemImages = new Image[numItemSlots];
-    public Item[] items = new Item[numItemSlots];
 
+    public Image[] itemImages= new Image[numItemSlots];
+    public Item[] items= new Item[numItemSlots];
 
     public const int numItemSlots = 4;
-
 
     public void AddItem(Item itemToAdd)
     {
         for (int i = 0; i < items.Length; i++)
         {
-            if (items[i] == null)
+            if (items[i]==null)
             {
                 items[i] = itemToAdd;
                 itemImages[i].sprite = itemToAdd.sprite;
@@ -24,8 +23,7 @@ public class Inventory : MonoBehaviour
         }
     }
 
-
-    public void RemoveItem (Item itemToRemove)
+    public void RemoveItem(Item itemToRemove)
     {
         for (int i = 0; i < items.Length; i++)
         {
